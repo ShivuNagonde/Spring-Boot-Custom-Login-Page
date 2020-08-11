c34d0f806eaf7a65748ac9c383154972e86bb0bc
Shivakumar <Shivakumar@DESKTOP-9PDBQK6>
2020-08-11 23:08:32 +0530

    fist commit uploading all files

---
+++ spring-boot-security-login-page/src/test/java/com/spring/sbsecureapp/SbSecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.sbsecureapp;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbSecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
